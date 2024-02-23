9ad15ef5637cf1e11f3dc52c5ca465bb74fcec2b
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-23 12:58:25 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import productsDb from './products.json'
+
+export default function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+
